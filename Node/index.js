b4996b4a5662221616e27342603ee5225299a2b3
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-04-10 20:24:47 +0530

    Created a PUT /users/:id route to fully update a user object.

--- Node/index.js
+++ Node/index.js
@@ -82,6 +82,35 @@ app.post('/users', (req, res) => {
     }
 });
 
+// PUT to Update Entire User
+// Task:
+// Use a PUT /users/:id route to fully update a user object.
+
+// Concepts Tested:
+
+// Full object replacement
+// Status codes (e.g., 404, 200)
+
+
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const { name, email } = req.body;
+  
+    const userIndex = users.findIndex(u => u.id === userId);
+  
+    if (userIndex === -1) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+  
+    if (!name || !email) {
+      return res.status(400).json({ error: 'Name and email are required.' });
+    }
+  
+ 
+    users[userIndex] = { id: userId, name, email };
+  
+    res.status(200).json(users[userIndex]);
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
