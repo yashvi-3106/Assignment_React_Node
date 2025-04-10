cc2795dbf2e4407652f72b9f5b4f496856a92562
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-04-10 15:06:24 +0530

    Created a route /users/:id that returns a user by ID.

--- Node/index.js
+++ Node/index.js
@@ -60,6 +60,29 @@ app.post('/users', (req, res) => {
   });
 
 
+//   GET User by ID
+// Task:
+// Create a route /users/:id that returns a user by ID.
+
+// Concepts Tested:
+
+// req.params usage
+// Conditional logic
+
+  app.get('/users/:id', (req, res) => {
+   
+    const id = parseInt(req.params.id);
+    
+    const user = users.find(user => user.id === id);
+    
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json({ message: 'User not found' });
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
